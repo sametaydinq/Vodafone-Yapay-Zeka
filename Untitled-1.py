81aa290cbc9d9da1c18b8a9aae8003bd0126357d
Samet Aydın <sametaydinq@gmail.com>
2023-10-14 13:05:56 +0300

    end of first day

---
+++ Untitled-1.py
@@ -0,0 +1,43 @@
+"""
+result = 21 % 9
+
+print(result)
+
+x = input("birinci :")
+y = input("ikinci :")
+z = input("üçüncü :")
+
+print("sayilarin toplami", int(x)+int(y)+int(z))
+
+"""
+
+"""print("Kurs Kayit Programi")
+
+ad = [input("Öğrencinin adı :")]
+soyad = [input("Öğrencinin soyadi :") ]
+
+bolum = [input("Okuduğu bölüm :")]
+
+Ogrenci_bilgisi = [ad, soyad, bolum]
+
+print("Ad: {} \nSoyad: {}\nBölüm: {}".format(Ogrenci_bilgisi[0],Ogrenci_bilgisi[1],Ogrenci_bilgisi[2]))
+
+"""
+"""
+
+sayi = int(input("sayi giriniz:"))
+
+if(sayi%2 ==0):
+    print("Sayi çift")
+else:
+    print("sayi tek")
+    
+    """
+    
+Kullanici_adi =  input("Kullanici adinizi giriniz:")
+Parola = input("şifrenizi giriniz:")
+
+if(Kullanici_adi=="samet" and Parola =="123"):
+    print("Giriş başarılı")
+else:
+    print("parolayı unuttum:")
---
+++ readme.md
@@ -0,0 +1,23 @@
+Gözetimli Öğrenme
+
+
+Sınıflandırma ve Tahmine Dayalı Algoritmalar
+Etiketli verilerle çalışılır. Daha güvenli sonuçlar almak için
+
+git
+Sınıflandırmada amaç bir karar üretebilmektir.
+
+K-means
+Random Forest
+Regression
+Decision Tree
+Linear & Logictic regression
+
+
+
+Sınıflandırmada çıktılar kategoriktir
+Regresyonda çıktılar sayısal verilerdir
+
+
+===Linear Regression===
+
